b6a360038d4172af4ee65192a91129b7d45efdb7
ulmaxy <ulmaxy@gmail.com>
2017-07-10 17:28:20 +0900

    Update MainActivity.java

--- app/src/main/java/com/ulmaxy/gitapp/MainActivity.java
+++ app/src/main/java/com/ulmaxy/gitapp/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d("TAG", "First branch");
         Log.d("TAG", "First branch2");
         Log.d("TAG", "bla bla");
+        Log.d("TAG", "some_branch");
     }
 }
